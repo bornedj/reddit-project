c9da8cb106deaf60b980cf039cae0ea8eaefdc38
bornedj <bornedj@miamioh.edu>
2021-07-08 09:08:54 -0400

    added the client id

--- src/app/App.js
+++ src/app/App.js
@@ -38,6 +38,13 @@ function App() {
         </ul>
       </nav>
 
+      <div>
+        <button 
+        onClick={()=> window.location = "https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&state=RANDOM_STRING&redirect_uri=URI&duration=DURATION&scope=SCOPE_STRING"}>
+          Authorize reddit
+        </button>
+      </div>
+
       <Switch>
         <Route path='/sports'>
           <Sports />
