edc903f47521a7c7d9a7f68c0df316ff4ecf6980
bornedj <bornedj@miamioh.edu>
2021-07-09 14:51:15 -0400

    have the grid setup working on upvotes

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './Tile.css'
 
 export default function Tile({props}) {
+    // get the necessary items from the props
     const title = props.title;
+
     const image = props.thumbnail ? props.thumbnail : 'no image';
+    let imageHeight;
+    let imageWidth;
+    if (image !== 'no image') {
+        imageHeight = props.thumbnail_height * 1.5;
+        imageWidth = props.thumbnail_width * 1.5;
+    }
     
+    const link = 'https://www.reddit.com' + props.permalink
+
     return (
         <div className="tile">
             <h6>{title}</h6>
-            <img src={image}></img>
+            <a href={link}><img src={image} style={{width: imageWidth, height: imageHeight}}></img></a>
         </div>
     )
 }
