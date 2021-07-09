b4c3af722b01dd47732c8753741f0fdbb101ba2f
bornedj <bornedj@miamioh.edu>
2021-07-09 15:06:10 -0400

    made the image the hyperlink, and formatted the upvotes

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,6 +5,7 @@ export default function Tile({props}) {
     // get the necessary items from the props
     const title = props.title;
 
+    // all image details
     const image = props.thumbnail ? props.thumbnail : 'no image';
     let imageHeight;
     let imageWidth;
@@ -12,13 +13,16 @@ export default function Tile({props}) {
         imageHeight = props.thumbnail_height * 1.5;
         imageWidth = props.thumbnail_width * 1.5;
     }
-    
+    // link to post
     const link = 'https://www.reddit.com' + props.permalink
+    // upvotes
+    const upvotes = props.ups; 
 
     return (
         <div className="tile">
             <h6>{title}</h6>
-            <a href={link}><img src={image} style={{width: imageWidth, height: imageHeight}}></img></a>
+            <a href={link} target='_blank'><img src={image} style={{width: imageWidth, height: imageHeight}}></img></a>
+            <h5>upvotes: {new Intl.NumberFormat('en-IN').format(upvotes)}</h5>
         </div>
     )
 }
