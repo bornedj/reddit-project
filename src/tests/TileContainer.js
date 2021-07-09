f4414f300402314e437d463a26c5c3a3391e03c0
bornedj <bornedj@miamioh.edu>
2021-07-09 19:58:52 -0400

    testing with mocha

---
+++ src/tests/TileContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import TileContainer from '../components/TileContainer/TileContainer';
+
+
+describe('testing seletPosts', () => {
+   it("returns an array of objects") 
+})
