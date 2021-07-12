fb3ff6ce08f9ea3c00def92ecc71a977d3f6ac29
bornedj <bornedj@miamioh.edu>
2021-07-12 13:23:26 -0400

    fixed the logic in select posts so not it returns an array of posts from different subreddits that are chosen randomly

--- src/features/fetch.js
+++ src/features/fetch.js
@@ -1,6 +1,3 @@
-// let url = "https://www.reddit.com/r/dankmemes/hot.json";
-// function will receive a link then go get the json from that link
-
 const fetchData =  async (url) => {
     const data = await fetch(url).then((response) => response.json());
     return data;
@@ -12,19 +9,15 @@ export const fetchReddits = async (props) => {
 
     for (let url of props) {
         const oneSubreddit = await fetchData(url);
-        // Object.entries(oneSubreddit.data.children).forEach(([idx, value]) => children.push(value))
-
-        console.log(oneSubreddit.data.children)
         children.push(oneSubreddit.data.children)
     }
-    console.log(children)
+
     return children;
 }
 
 export const selectPosts = async (props) => {
     const postsToShow = [];
     const subredditsData = await fetchReddits(props);
-    console.log(subredditsData)
 
     //while loop to select 32 posts to show
     do {
@@ -32,8 +25,9 @@ export const selectPosts = async (props) => {
             //get value from random index in array
             let idx = Math.floor(Math.random() * subredditsData[i].length);
             let dataToAdd = subredditsData[i][idx];
-            subredditsData[i] = subredditsData[i].splice(idx, 1);
-            // console.log(subredditsData[i].length)
+            console.log(subredditsData[i].length)
+            subredditsData[i].splice(idx, 1);
+            console.log(subredditsData[i].length)
             postsToShow.push(dataToAdd);
         }
     } while (postsToShow.length < 33);
